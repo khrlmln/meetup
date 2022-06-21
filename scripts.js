b0e65e5ae8b617867a7ab3b9c32a2a63731b0996
Milan Kharel <khrlmln66@gmail.com>
2022-06-21 18:16:17 +0545

    initial commit

---
+++ scripts.js
@@ -0,0 +1,23 @@
+const menuTrigger = document.querySelector(".menu-trigger"),
+  closeTrigger = document.querySelector(".close"),
+  giveClass = document.querySelector(".site");
+
+menuTrigger.addEventListener("click", function () {
+  giveClass.classList.toggle("showmenu");
+});
+closeTrigger.addEventListener("click", function () {
+  giveClass.classList.remove("showmenu");
+});
+
+// animated items on scroll
+const io = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.intersectionRatio > 0) {
+      entry.target.classList.add("this");
+    }
+  });
+});
+const boxEllist = document.querySelectorAll(".animate");
+boxEllist.forEach((el) => {
+  io.observe(el);
+});
